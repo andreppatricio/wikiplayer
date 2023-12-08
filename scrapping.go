b7a678a96b5fcfb95b10872afe349a88a37d697e
André Patrício <andremppatricio@gmail.com>
2023-12-08 09:46:51 +0000

    Add support for input as end of wikipedia link

--- scrapping.go
+++ scrapping.go
@@ -10,6 +10,8 @@ import (
 )
 
 var prefix string = "https://en.wikipedia.org"
+var prefix_wiki string = "https://en.wikipedia.org/wiki/"
+var section string = "#mw-content-text"
 
 func getQuote() string {
 	return quote.Go()
@@ -17,31 +19,41 @@ func getQuote() string {
 
 func getWikiLinks(visit string) []string {
 
-	section := "#mw-content-text"
+	short_mode := !strings.HasPrefix(visit, prefix_wiki)
+
 	c := colly.NewCollector()
 	c.SetRequestTimeout(30 * time.Second)
 
 	links := []string{}
-	// linkSet := make(Set)
 
 	// Find and visit all links
 	c.OnHTML(section+" a[href]", func(e *colly.HTMLElement) {
 		// Ignore subsections References and Final Tables
-
 		if e.DOM.ParentsFiltered(".reflist").Is("div") || e.DOM.ParentsFiltered(".navbox").Is("div") {
 			return
 		}
 
 		link := e.Attr("href")
 		if strings.HasPrefix(link, "/wiki/") && !strings.Contains(link, ":") {
-			if !strings.HasPrefix(link, prefix) {
+			// if !strings.HasPrefix(link, prefix) {
+			// 	link = prefix + link
+			// }
+			// links = append([]string{link}, links...)
+			if short_mode {
+				link = strings.TrimPrefix(link, "/wiki/")
+			} else {
 				link = prefix + link
 			}
-			// links = append([]string{link}, links...)
+			// fmt.Println(link)
 			links = append(links, link)
 		}
 	})
 
+	if short_mode {
+		visit = prefix_wiki + visit
+	}
+	// fmt.Println("Visiting: ", visit)
+	// fmt.Scanln()
 	err := c.Visit(visit)
 	if err != nil {
 		log.Fatal(err)
