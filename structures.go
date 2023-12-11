cfd22ca568ce74b673c39bbf5a89b77896fbb813
André Patrício <andremppatricio@gmail.com>
2023-12-11 12:07:40 +0000

    Add concurrent Queue

--- structures.go
+++ structures.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"slices"
+	"sync"
 )
 
 // Stack Implementation
@@ -82,4 +83,39 @@ func (q *Queue) IsEmpty() bool {
 
 func (q *Queue) Size() int {
 	return len(q.elements)
+}
+
+// Concurrency Queue Implementation
+type CQueue struct {
+	items []string
+	lock  sync.Mutex
+}
+
+func (q *CQueue) Add(item string) {
+	q.lock.Lock()
+	defer q.lock.Unlock()
+	q.items = append(q.items, item)
+}
+
+func (q *CQueue) AddAll(items ...string) {
+	q.lock.Lock()
+	defer q.lock.Unlock()
+	q.items = append(q.items, items...)
+}
+
+func (q *CQueue) Pop() string {
+	q.lock.Lock()
+	defer q.lock.Unlock()
+	if len(q.items) == 0 {
+		return ""
+	}
+	item := q.items[0]
+	q.items = q.items[1:]
+	return item
+}
+
+func (q *CQueue) IsEmpty() bool {
+	q.lock.Lock()
+	defer q.lock.Unlock()
+	return len(q.items) == 0
 }
